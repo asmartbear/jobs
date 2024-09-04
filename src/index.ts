cbbc817a5c85b8ba03ce2848c7afb851b2f279cc
Jason Cohen <108685+asmartbear@users.noreply.github.com>
2024-09-04 09:48:14 -0500

    Constructors with types; tags that get displayed

--- src/index.ts
+++ src/index.ts
@@ -10,54 +10,103 @@ export enum TaskState {
   Error
 }
 
+export type TaskRunnerConstructor = {
+
+  /**
+   * The concurrency level, or undefined to use the number of CPUs available.
+   */
+  concurrencyLevel?: number,
+
+  /**
+   * Log on the console when the whole runner starts?
+   */
+  logRunnerStart?: boolean
+
+  /**
+   * Log on the console when the whole runner completes?
+   */
+  logRunnerEnd?: boolean
+
+  /**
+   * Log on the console when a task begins?
+   */
+  logTaskStart?: boolean
+
+  /**
+   * Log on the console when a task completes?
+   */
+  logTaskEnd?: boolean
+}
+
 /**
  * Options when creating a new `Task`.
  */
-export type TaskConstructor = {
+export type TaskConstructor<Tags extends string> = {
+
   /**
    * Used for human-readable output.
    */
   title: string,
+
   /**
    * Tasks will run in priority-order, from lowest to highest, though different priorities can be running concurrently.
    */
+
   priority?: number,
+
   /**
    * Don't start this task unless all tasks with a lower completionPriority have not only started, but finished.
    */
   completionPriority?: number,
+
+  /**
+   * Tags to apply to this task.
+   */
+  tags?: Tags[]
 }
 
 /**
  * Runnable Task.
  */
-export class Task {
+export class Task<Tags extends string> {
+
   /**
    * Used for human-readable output.
    */
   public readonly title: string
+
   /**
    * Tasks will run in priority-order, from lowest to highest, though different priorities can be running concurrently.
    */
   public readonly priority: number
+
   /**
    * Don't start this task unless all tasks with a lower completionPriority have not only started, but finished.
    */
   public readonly completionPriority: number
 
   /**
+   * Tags that are applied to this task.
+   */
+  public readonly tags: Tags[]
+
+  /**
    * The current state of the Task.
    */
   public state: TaskState = TaskState.New;
-  private readonly dependentTasks: Task[] = [];
+  private readonly dependentTasks: Task<Tags>[] = [];
 
-  constructor(config: TaskConstructor, private readonly executionFunction: () => Promise<void>) {
+  constructor(config: TaskConstructor<Tags>, private readonly executionFunction: () => Promise<void>) {
     this.title = config.title
     this.priority = config.priority ?? 0
     this.completionPriority = config.completionPriority ?? 0
+    this.tags = config.tags ?? []
+    if (this.tags.length > 0) {
+      this.title += " [" + this.tags.join(", ") + "]"
+    }
   }
 
-  isReady(readyQueue: Task[], runningTasks: Task[]): boolean {
+  isReady(readyQueue: Task<Tags>[], runningTasks: Task<Tags>[]): boolean {
     if (this.state !== TaskState.New) return false;
 
     // Check task list dependency
@@ -90,7 +139,7 @@ export class Task {
   /**
    * Declares that we're dependent on the completion of some other task.
    */
-  addDependentTask(task: Task): void {
+  addDependentTask(task: Task<Tags>): void {
     this.dependentTasks.push(task);
   }
 }
@@ -98,22 +147,23 @@ export class Task {
 /**
  * A Task-running system.  Load initial tasks; tasks can beget other tasks.  Can run until all are complete.
  */
-export class ExecutionManager {
-  private queue: Task[] = [];
-  private runningTasks: Task[] = [];
-  private doneTasks: Task[] = [];
+export class TaskRunner<Tags extends string> {
+  private queue: Task<Tags>[] = [];
+  private runningTasks: Task<Tags>[] = [];
+  private doneTasks: Task<Tags>[] = [];
   private _error: Error | null = null;
-  private concurrencyLevel: number;
 
-  constructor(concurrencyLevel: number, public readonly verbose: boolean) {
-    this.concurrencyLevel = concurrencyLevel <= 0 ? getNumCpus().length : concurrencyLevel;
+  constructor(public readonly config: TaskRunnerConstructor) {
+    // nothing else to do
   }
 
   /**
    * Enqueues a task to run.
    */
-  addTask(task: Task): void {
+  addTask<TheseTags extends Tags>(config: TaskConstructor<TheseTags>, executionFunction: () => Promise<void>): Task<TheseTags> {
+    const task = new Task(config, executionFunction)
     this.queue.push(task);
+    return task
   }
 
   /**
@@ -122,12 +172,13 @@ export class ExecutionManager {
    */
   async run(): Promise<void> {
     const tStart = Date.now();
-    if (this.verbose) {
-      console.log(`Jobs starting; pid=${process.pid}; concurrency=${this.concurrencyLevel}`);
+    const concurrencyLevel = this.config.concurrencyLevel ?? getNumCpus().length;
+    if (this.config.logRunnerStart) {
+      console.log(`Jobs starting; pid=${process.pid}; concurrency=${concurrencyLevel}`);
     }
-    const workers = Array(this.concurrencyLevel).fill(null).map(() => this.worker());
+    const workers = Array(concurrencyLevel).fill(null).map(() => this.worker());
     await Promise.all(workers);
-    if (this.verbose) {
+    if (this.config.logRunnerEnd) {
       console.log(`Jobs finished; pid=${process.pid}; ${this.doneTasks.length} tasks completed in ${Math.ceil((Date.now() - tStart) / 1000)}s`);
     }
   }
@@ -147,14 +198,14 @@ export class ExecutionManager {
 
       // Run the task
       const task = this.queue.splice(readyTaskIndex, 1)[0];
-      if (this.verbose) {
-        // console.log(`>>> ${task.title}`)
+      if (this.config.logTaskStart) {
+        console.log(`>>> ${task.title}`)
       }
       this.runningTasks.push(task);
       const tStart = Date.now()
       const error = await task.execute();
       const tDuration = Date.now() - tStart
-      if (this.verbose) {
+      if (this.config.logTaskEnd) {
         console.log(`<<< ${task.title} in ${tDuration}ms ${error ? ` (ERR: ${error.message})` : ""}`);
       }
       this.runningTasks = this.runningTasks.filter(t => t !== task);
@@ -174,42 +225,56 @@ export class ExecutionManager {
 }
 
 // Usage example
-// async function main() {
-//   const manager = new ExecutionManager(3, true);
+async function main() {
+  const manager = new TaskRunner<"foo" | "bar">({
+    concurrencyLevel: 2,
+    logRunnerStart: true,
+    logRunnerEnd: true,
+    logTaskStart: true,
+    logTaskEnd: true,
+  });
 
-//   const task1 = new Task({
-//     title: "Task 1"
-//   }, async () => {
-//     await new Promise(resolve => setTimeout(resolve, 1000));
-//   });
+  const task1 = manager.addTask({
+    title: "Task 1",
+    priority: 1,
+    tags: ["foo"],
+  }, async () => {
+    await new Promise(resolve => setTimeout(resolve, 500));
+  });
 
-//   const task2 = new Task({
-//     title: "Task 2"
-//   }, async () => {
-//     await new Promise(resolve => setTimeout(resolve, 1500));
-//     // throw new Error("Task 2 failed");
-//   });
+  const task2 = manager.addTask({
+    title: "Task 2",
+    priority: 2,
+    tags: ["foo"],
+  }, async () => {
+    await new Promise(resolve => setTimeout(resolve, 750));
+    // throw new Error("Task 2 failed");
+  });
 
-//   const task3 = new Task({
-//     title: "Task 3"
-//   }, async () => {
-//     await new Promise(resolve => setTimeout(resolve, 500));
-//   });
+  const task3 = manager.addTask({
+    title: "Task 3",
+    priority: 3,
+  }, async () => {
+    await new Promise(resolve => setTimeout(resolve, 250));
+  });
 
-//   // task1.addDependentTask(task3);
-//   // task2.addDependentTask(task3);
+  const task4 = manager.addTask({
+    title: "Task 4",
+    priority: 4,
+  }, async () => {
+    await new Promise(resolve => setTimeout(resolve, 1));
+  });
 
-//   manager.addTask(task1);
-//   manager.addTask(task2);
-//   manager.addTask(task3);
+  // task1.addDependentTask(task3);
+  // task2.addDependentTask(task3);
 
-//   await manager.run();
+  await manager.run();
 
-//   if (manager.error) {
-//     console.error("An error occurred:", manager.error);
-//   } else {
-//     console.log("All tasks completed successfully");
-//   }
-// }
+  if (manager.error) {
+    console.error("An error occurred:", manager.error);
+  } else {
+    console.log("All tasks completed successfully");
+  }
+}
 
-// main().catch(console.error);
+main().catch(console.error);
